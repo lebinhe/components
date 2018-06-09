b079c7527fe3497e92bd6eddc20279510ebbaac2
lebinhe <iceriver12@gmail.com>
2018-06-09 23:22:10 +0800

    add logging

---
+++ util/logging.h
@@ -0,0 +1,30 @@
+#ifndef COMPONENTS_UTIL_LOGGING_H_
+#define COMPONENTS_UTIL_LOGGING_H_
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string>
+
+class Slice;
+
+// Append a human-readable printout of "num" to *str
+void AppendNumberTo(std::string* str, uint64_t num);
+
+// Append a human-readable printout of "value" to *str.
+// Escapes any non-printable characters found in "value".
+void AppendEscapedStringTo(std::string* str, const Slice& value);
+
+// Return a human-readable printout of "num"
+std::string NumberToString(uint64_t num);
+
+// Return a human-readable version of "value".
+// Escapes any non-printable characters found in "value".
+std::string EscapeString(const Slice& value);
+
+// Parse a human-readable number from "*in" into *value.  On success,
+// advances "*in" past the consumed number and sets "*val" to the
+// numeric value.  Otherwise, returns false and leaves *in in an
+// unspecified state.
+bool ConsumeDecimalNumber(Slice* in, uint64_t* val);
+
+#endif // COMPONENTS_UTIL_LOGGING_H_
